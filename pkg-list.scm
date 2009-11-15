64d14ce89c61a8e71b3ac06fd4a7f34cd644fb99
Andreas Rottmann <a.rottmann@gmx.at>
2009-11-15 17:26:48 +0100

    Added `pkg-list.scm'

---
+++ pkg-list.scm
@@ -0,0 +1,9 @@
+(package (ocelotl (0))
+  (depends (srfi)
+           (spells)
+           (parscheme))
+  (libraries (libs -> "ocelotl")))
+
+;; Local Variables:
+;; scheme-indent-styles: ((package 1))
+;; End:
