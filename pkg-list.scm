3a0e7b217de8654aaee637d122cd3d781a7c91c4
Andreas Rottmann <a.rottmann@gmx.at>
2010-04-14 22:53:14 +0200

    pkg-list.scm: split out soup-httpd.sls since it depends on sbank

--- pkg-list.scm
+++ pkg-list.scm
@@ -2,7 +2,18 @@
   (depends (srfi)
            (spells)
            (parscheme))
-  (libraries (libs -> "ocelotl")))
+  (libraries
+   (exclude ("net" "soup-httpd.sls"))
+   (sls -> "ocelotl")
+   (("net" "private") -> ("ocelotl" "net" "private"))))
+
+(package (ocelotl-soup (0))
+  (depends (srfi)
+           (spells)
+           (sbank)
+           (ocelotl))
+  (libraries
+   (("net" "soup-httpd.sls") -> ("ocelotl" "net" "soup-httpd.sls"))))
 
 ;; Local Variables:
 ;; scheme-indent-styles: ((package 1))
